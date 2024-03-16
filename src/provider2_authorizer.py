c521b2aa295cac7891ac90ba2887ba8c61898bca
thezeroend <matheuslol150@gmail.com>
2024-03-16 16:12:45 -0300

    Remove coments

--- src/provider2_authorizer.py
+++ src/provider2_authorizer.py
@@ -2,7 +2,4 @@ from src.authorizer_generic import AuthorizerGeneric
 
 class Provider2Authorizer(AuthorizerGeneric):
     def authorize(self):
-        # Implemente a lógica de autorização específica para o provider 2
-        # Use self.token_info para acessar as informações do token, se necessário
-        # Retorna True se o acesso for permitido e False caso contrário
-        return False  # Exemplo: nunca permitir acesso para demonstração
+        return False
