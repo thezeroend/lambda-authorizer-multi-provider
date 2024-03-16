0061837f2e963bed8de89fcbcb671d9f02784940
thezeroend <matheuslol150@gmail.com>
2024-03-16 15:50:16 -0300

    Comentarios

--- src/decorators.py
+++ src/decorators.py
@@ -7,7 +7,6 @@ def log_class_and_method(log_message="Executando", finish_message="Finalizada"):
         def wrapper(*args, **kwargs):
             class_name = args[0].__class__.__name__ 
             method_name = func.__name__
-
             logging.info("[%s][%s] - %s", class_name, method_name, log_message)
             result = func(*args, **kwargs)
             logging.info("[%s][%s] - %s", class_name, method_name, finish_message)
